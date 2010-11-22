bf3fc02d82e97f9f8f2df1b347baeb09dcd62fd2
Kenji_Takahashi <wozniakk@gmail.com>
2010-11-23 00:48:39 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RoRZad7::Application.config.secret_token = 'de83af52456c1b3bcfd4c15fff295f40d2f6094ebc7730a16cc0461b8e4c8d6bc9928342c285060d286185f3c1b0648e899948e9ed0adf9740a2649eafe7e07e'
